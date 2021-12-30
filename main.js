6f35296a205fdf6d7d9a5d9e1a79d3cf6c74ccdc
RKcoding <73450077+RKcoding@users.noreply.github.com>
2021-12-29 20:16:34 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = "";
 
 function setup() {
     canvas = createCanvas(300, 300);
@@ -21,4 +22,31 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    if (status != empty) {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "The number of objects detected are:" + objects.length;
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        if (objects[i].label == Object Name) {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_status").innerHTML = Object Name + " found";
+            synth = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance(Object Name + " found");
+            synth.speak(utterThis);
+        } else {
+            document.getElementById("object_status").innerHTML = Object Name + " not found";
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
